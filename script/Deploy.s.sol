a992efd3f2eb1fa371c44a99bf85e420c9c63e8e
jgeary <36774175+jgeary@users.noreply.github.com>
2023-10-04 16:26:37 -0400

    goerli deploy 1 (#5)

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19 <=0.9.0;
 
-// import { Foo } from "../src/Foo.sol";
+import { Script } from "forge-std/Script.sol";
+import { console2 } from "forge-std/console2.sol";
+import { ERC1967Proxy } from "openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
+import { PixieChessAuctionMinter } from "../src/PixieChessAuctionMinter.sol";
+import { PixieChessToken } from "../src/PixieChessToken.sol";
 
-import { BaseScript } from "./Base.s.sol";
+contract Deploy is Script {
+    function run() public {
+        address deployer = vm.envAddress("DEPLOYER");
+        address payable multisig = payable(vm.envAddress("MULTISIG"));
 
-/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
-contract Deploy is BaseScript {
-    function run() public broadcast {
-        // foo = new Foo();
+        vm.startBroadcast(deployer);
+
+        PixieChessToken tokenImpl = new PixieChessToken();
+        PixieChessToken token = PixieChessToken(address(new ERC1967Proxy(address(tokenImpl), "")));
+        token.initialize(multisig);
+
+        PixieChessAuctionMinter auction = new PixieChessAuctionMinter(multisig, address(token));
+
+        vm.stopBroadcast();
+
+        console2.log("PixieChessToken implementation deployed at: ", address(tokenImpl));
+        console2.log("PixieChessToken deployed at: ", address(token));
+        console2.log("PixieChessAuctionMinter deployed at: ", address(auction));
+
+        // manually (for now) grant the token minter role to the auction contract
     }
 }
